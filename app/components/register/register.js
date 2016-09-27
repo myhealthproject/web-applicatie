dba8052b528e04a4cb869a8075603fe320d6f764
Arwyn Boiten <arwynboiten475@gmail.com>
2016-09-27 16:55:25 +0200

    Added register view.

--- app/components/register/register.js
+++ app/components/register/register.js
@@ -2,13 +2,21 @@
 
 angular.module('myHealth.register', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/register', {
-    templateUrl: './app/components/register/register.html',
-    controller: 'RegisterCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/register', {
+        templateUrl: './app/components/register/register.html',
+        controller: 'RegisterCtrl'
+      });
+    }])
 
-.controller('RegisterCtrl', [function() {
+    .controller('RegisterCtrl', ['$scope', function($scope) {
+      $scope.sendRegisterForm = function (user) {
+        var username = $scope.createUsername(user);
+        alert("Username: " + username);
+      }
 
-}]);
+      $scope.createUsername = function(user){
+        var username = user.firstname.substr(0, 1) + "." + user.lastname;
+        return username;
+      }
+    }]);
