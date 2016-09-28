23f24121f713e926e2f729f52969a6d49c93d4c8
Arwyn Boiten <arwynboiten475@gmail.com>
2016-09-28 21:39:45 +0200

    Added user registration methods.

--- app/components/register/registerController.js
+++ app/components/register/registerController.js
@@ -2,14 +2,54 @@
 
 angular.module('myHealth.registerCtrl', ['ngRoute'])
 
-    .controller('RegisterCtrl', ['$scope', function($scope) {
-      $scope.sendRegisterForm = function (user) {
-        var username = $scope.createUsername(user);
-        alert("Username: " + username);
+    .controller('RegisterCtrl', ['$scope', '$http', function($scope, $http) {
+      $scope.sendRegisterForm = function (user, form) {
+          var error = '';
+          if(form.$valid) {
+
+              var req = {
+                  method: 'POST',
+                  url: 'http://henkdieter.com/api/user',
+                  headers: {
+                      'Content-Type': 'application/json '
+                  },
+                  data: {
+                      'uname': user.username,
+                      'fname': user.firstname,
+                      'lname': user.lastname,
+                      'password': user.password
+                  }
+              };
+
+              $http(req).then(function callBack(response) {
+                  if (response._id != "") {
+                      alert("Er is een nieuwe gebruiker aangemaakt.");
+                      $scope.user = null;
+                  }
+              }, function error(response) {
+                  alert('Response: ' + response);
+              });
+          } else {
+              if(!form.firstname.$valid){
+                  error += "Voornaam. \n";
+              }
+              if(!form.lastname.$valid){
+                  error += "Achternaam \n";
+              }
+              if(!form.username.$valid){
+                  error += "Username. \n";
+              }
+              if(!form.password.$valid){
+                  error += "Wachtwoord. \n";
+              }
+              if(error != ""){
+                  alert("De volgende velden ontbreken: \n" + error);
+              }
+          }
       }
 
       $scope.createUsername = function(user){
-        var username = user.firstname.substr(0, 1) + "." + user.lastname;
-        return username;
+        var username = user.firstname.substr(0, 1)  + user.lastname;
+          $scope.user.username = username;
       }
     }]);
