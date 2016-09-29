b5d7340c6ebb0696b741574afc285c5ccaf7fbbe
ghostbirt <jeroenartsneev@hotmail.com>
2016-09-29 10:15:53 +0200

    login

--- app/components/login/loginController.js
+++ app/components/login/loginController.js
@@ -5,6 +5,53 @@ angular.module('myHealth.loginCtrl', ['ngRoute'])
 
 .controller('LoginCtrl', ['$scope', function($scope) {
   $scope.sendLoginForm = function (user) {
-    alert("Username: " + user.username + "\n" + "Password: " + user.password);
+      var error = '';
+
+     if(form.$valid) {
+
+         var req = {
+             method: 'Post',
+             url: 'http://henkdieter.com/api/login',
+             headers: {
+                 'Content-Type': 'application/json '
+             },
+             data: {
+                 'uname': user.username,
+                 'password': user.password
+             }
+         };
+         $http(req).then(function callBack(response) {
+             if (response.success) {
+                 $window.location.href='/profile';
+
+                 alert(response)
+             }
+         }, function error(response) {
+             alert('Response: ' + response.message);
+         });
+
+     } else {
+
+
+        if(!form.username.$valid){
+            error += "Username. \n";
+        }
+        if(!form.password.$valid){
+            error += "Wachtwoord. \n";
+        }
+        if(error != ""){
+            alert("De volgende velden ontbreken: \n" + error);
+        }
+    }
+
+
+
+
+
+    alert("Error: " + error + "\n");
   };
-}]);
+}])
+
+
+
+
