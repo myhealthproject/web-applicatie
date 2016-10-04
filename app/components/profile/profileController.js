4c04276af75d847f4c309c184095efe55bf832a1
ghostbirt <jeroenartsneev@hotmail.com>
2016-10-04 13:25:51 +0200

    Tableview voor billView
    Aanvullingen aan profile

--- app/components/profile/profileController.js
+++ app/components/profile/profileController.js
@@ -9,8 +9,62 @@ angular.module('myHealth.profileCtrl', ['ngRoute'])
         });
     }])
 
+    .controller('ProfileCtrl', ['$scope', '$http', function($scope, $http, $location) {
+        $scope.textBtnForm = function (user, form) {
+            var error = '';
+
+            if (form.$valid) {
+
+                var req = {
+                    method: 'Post',
+                    url: 'http://henkdieter.com/api/profile',
+                    headers: {
+                        'Content-Type': 'application/json '
+                    },
+                    data: {
+                        'fname': user.firstname,
+                        'lname': user.lastname,
+                        'password':user.password
+
+
+                    }
+                };
+
+                $http(req).then(function callBack(response) {
+                    if (response.data.success) {
+                        alert('Gebruikers ingelogd.');
+
+                    } else {
+                        alert("Fout: " + response.data.message);
+                    }
+                }, function error(response) {
+                    alert('Response: ' + response.message);
+                });
+
+            } else {
+
+
+                if (!form.firstname.$valid) {
+                    error += "Firstname. \n";
+                }
+                if (!form.lastname.$valid) {
+                    error += "Lastname. \n";
+                }
+                if (!form.lastname.$valid) {
+                    error += "Wachtwoord. \n";
+                }
+                if (error != "") {
+                    alert("De volgende velden ontbreken: \n" + error);
+                }
+            }
+        };
+    }])
+
+
+
     .controller('ProfileCtrl', ['$scope', function($scope) {
         $scope.user = {
-            name: 'Arwyn Boiten'
+            firstname: 'Arwyn',
+            lastname: 'boyten'
         };
     }]);
