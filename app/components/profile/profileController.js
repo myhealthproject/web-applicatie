e36489c1307e5f928667117b245684a5df9abdbc
HartgerV <hartgerveeman@gmail.com>
2016-10-06 10:59:23 +0200

    Profile edit (yolocode)

--- app/components/profile/profileController.js
+++ app/components/profile/profileController.js
@@ -2,7 +2,36 @@
 
 angular.module('myHealth.profileCtrl', ['ngRoute'])
 
-    .controller('ProfileCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+    .controller('ProfileCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
+        $scope.showEdit = false;
+
+        $scope.toggleEdit = function() {
+            if($scope.showEdit) {
+                $scope.showEdit = false;
+            } else {
+                $scope.showEdit = true;
+            }
+        };
+
+        $scope.edit = function() {
+            var req = {
+                method: 'Put',
+                url: 'http://henkdieter.com/api/user/' + $rootScope.globals.currentUser.user._id,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: {
+                    'fname': $scope.user.firstname,
+                    'lname': $scope.user.lastname
+                }
+            };
+            $http(req).success(function (response) {
+                callback(response);
+            });
+            $rootScope.globals.currentUser.user.fname = $scope.user.firstname;
+            $rootScope.globals.currentUser.user.lname = $scope.user.lastname;
+            $scope.toggleEdit();
+        };
         $scope.user = {
             uname: $rootScope.globals.currentUser.user.uname,
             firstname: $rootScope.globals.currentUser.user.fname,
